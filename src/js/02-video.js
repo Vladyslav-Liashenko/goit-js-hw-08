78ef628e0b874e1de5a012ecd068f34643f621ad
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-09-15 16:25:46 +0300

    save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on('timeupdate', event => {
+  const currentTime = event.target.currentTime;
+  // Зберігайте час відтворення у локальному сховищі
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+});
+
+// Отримати збережений час відтворення з локального сховища
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+// Перевірка, чи є збережений час і чи він є числом
+if (savedTime !== null && !isNaN(savedTime)) {
+  // Перетворити збережений час у число
+  const currentTime = parseFloat(savedTime);
+
+  // Встановити збережений час відтворення
+  player.setCurrentTime(currentTime);
+} else {
+  // Якщо збережений час відсутній або некоректний, робити щось інше (наприклад, починати від початку)
+  // player.setCurrentTime(0); // Наприклад, почати від початку
+}
+
+// Тепер ви можете відтворити відео
+player.play();
+
 
