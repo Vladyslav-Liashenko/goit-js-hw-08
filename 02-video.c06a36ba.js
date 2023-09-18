2132a1e781bec32e82e9fa10297724098dd08047
Vladyslav-Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-09-18 13:58:07 +0000

    Deploying to gh-pages from @ Vladyslav-Liashenko/goit-js-hw-08@22c0e5a871cfbe0918bb35e42d98bbb354401d30 🚀

--- 02-video.c06a36ba.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){console.log("title:",e)})),t.on("timeupdate",(function(e){var t=e.target.currentTime;localStorage.setItem("videoplayer-current-time",t.toString())}));var o=localStorage.getItem("videoplayer-current-time");if(null!==o&&!isNaN(o)){var r=parseFloat(o);t.setCurrentTime(r)}t.play()}();
-//# sourceMappingURL=02-video.c06a36ba.js.map
--- 02-video.fddcc135.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){console.log("played the video!")})),t.getVideoTitle().then((function(e){console.log("title:",e)})),t.on("timeupdate",(e=>{const t=e.target.currentTime;localStorage.setItem("videoplayer-current-time",t.toString())}));const o=localStorage.getItem("videoplayer-current-time");if(null!==o&&!isNaN(o)){const e=parseFloat(o);t.setCurrentTime(e)}t.play();
-//# sourceMappingURL=02-video.fddcc135.js.map
