1bd91b243d9c418fe4f0e39178bdbd6007d931b3
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-09-21 12:06:21 +0300

    save

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -9,53 +9,26 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
 
-const galleryContainer = document.querySelector(".gallery");
+const galleryContainer = document.querySelector('.gallery');
 const galleryElement = [];
 
-galleryItems.forEach((item) => {
-  const galleryItemElement = document.createElement("li");
-  galleryItemElement.className = "gallery__item";
-  const link = document.createElement("a");
-  link.className = "gallery__link";
-  link.setAttribute("href", item.original);
-  const imageElement = document.createElement("img");
+galleryItems.forEach(item => {
+  const galleryItemElement = document.createElement('li');
+  galleryItemElement.className = 'gallery__item';
+  const link = document.createElement('a');
+  link.className = 'gallery__link';
+  link.setAttribute('href', item.original);
+  const imageElement = document.createElement('img');
   imageElement.src = item.preview;
   imageElement.alt = item.description;
-  imageElement.setAttribute("data-src", item.original);
-  imageElement.className = "gallery__image";
+  imageElement.setAttribute('data-src', item.original);
+  imageElement.className = 'gallery__image';
   galleryItemElement.appendChild(link);
   link.appendChild(imageElement);
   galleryElement.push(galleryItemElement);
 });
 galleryContainer.append(...galleryElement);
 
-galleryContainer.addEventListener("click", openmodal);
-function openmodal(event) {
-  event.preventDefault();
-  if (event.target.nodeName === "IMG") {
-    const src = event.target.dataset.src;
-    const instance = basicLightbox.create(
-      `
-      <div class="modal">
-          <img src="${src}" alt="${event.target.alt} href="${src}">
-      </div>
-  `,
-      {
-        onShow: (instance) => {
-          document.addEventListener("keydown", keyCodeEvent);
-        },
-        onClose: (instance) => {
-          document.removeEventListener("keydown", keyCodeEvent);
-        }
-      }
-    );
-    instance.show();
-    function keyCodeEvent(event) {
-      if (event.keyCode === 27) {
-        instance.close();
-      }
-    }
-  };
-};
-console.log(galleryItems);
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 
+console.log(galleryItems);
