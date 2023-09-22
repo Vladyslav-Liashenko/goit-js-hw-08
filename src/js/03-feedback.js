decb568ca2411d096adeed3e8359be3c927e4bb3
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2023-09-22 13:39:54 +0300

    save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+const feedbackStorageKey = 'feedback-form-state';
+
+
+const updateStorage = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem(feedbackStorageKey, JSON.stringify(formState));
+}, 500);
+
+
+function fillFormFromStorage() {
+  const storedState = localStorage.getItem(feedbackStorageKey);
+  if (storedState) {
+    const formState = JSON.parse(storedState);
+    emailInput.value = formState.email || '';
+    messageTextarea.value = formState.message || '';
+  }
+}
+fillFormFromStorage();
+
+feedbackForm.addEventListener('input', updateStorage);
+
+feedbackForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formState = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  // Очищаємо сховище
+  localStorage.removeItem(feedbackStorageKey);
+
+  // Виводимо дані в консоль
+  console.log('Form submitted with data:', formState);
+
+  // Очищаємо поля форми
+  emailInput.value = '';
+  messageTextarea.value = '';
+});
